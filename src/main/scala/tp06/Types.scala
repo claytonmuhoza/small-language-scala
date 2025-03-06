6ab48e700bc14d7ac0ba7bb3e53f040c9bf1b324
MUHOZA Clayton <muhozaclayton@gmail.com>
2025-03-06 10:55:05 +0100

    add abs and app to types

--- src/main/scala/tp06/Types.scala
+++ src/main/scala/tp06/Types.scala
@@ -4,3 +4,6 @@ sealed trait Term
 case object EOF extends Term
 case class Var(name : String) extends Term
 case class Val(x : Var, t : Term) extends Term
+case class Abs(x : Var, t : Term) extends Term
+case class App(t1 : Term, t2 : Term) extends Term
+
